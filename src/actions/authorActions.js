a5753f02ad491c3389923ebbcdafa77bbffa2c35
Mário Chaves <macndesign@gmail.com>
2016-09-29 00:51:22 -0300

    pre-loader

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -3,6 +3,7 @@
  */
 import AuthorApi from '../api/mockAuthorApi';
 import * as types from './actionTypes';
+import { beginAjaxCall } from './ajaxStatusActions';
 
 export function loadAuthorSuccess(authors) {
   return {type: types.LOAD_AUTHORS_SUCCESS, authors}
@@ -10,6 +11,7 @@ export function loadAuthorSuccess(authors) {
 
 export function loadAuthors() {
   return dispatch => {
+    dispatch(beginAjaxCall());
     return AuthorApi.getAllAuthors().then(authors => {
       dispatch(loadAuthorSuccess(authors));
     }).catch(error => {
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,7 +5,7 @@ import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
 import LoadingDots from './LoadingDots';
 
-const Header = () => {
+const Header = ({loading}) => {
   return (
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
@@ -13,9 +13,13 @@ const Header = () => {
       <IndexLink to="/courses" activeClassName="active">Courses</IndexLink>
       {" | "}
       <IndexLink to="/about" activeClassName="active">About</IndexLink>
-      <LoadingDots interval={100} dots={20}/>
+      {loading && <LoadingDots interval={100} dots={20}/>}
     </nav>
   );
+};
+
+Header.propTypes = {
+  loading: PropTypes.bool.isRequired
 };
 
 export default Header;
